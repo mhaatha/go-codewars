480295747f31fb77f7a71c9f33f7f4307e9a49ef
mhaatha <mhafidza528@gmail.com>
2025-01-01 10:27:26 +0800

    Move Sleeper interface and DefaultSleeper struct to different file

--- Mocking/countdown_test.go
+++ Mocking/countdown_test.go
@@ -3,26 +3,7 @@ package main
 import (
 	"bytes"
 	"testing"
-	"time"
 )
-
-type Sleeper interface {
-	Sleep()
-}
-
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
-}
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
