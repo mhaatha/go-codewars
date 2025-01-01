598b821fd24629d1198de24850dd0a3e3d7c3ac8
mhaatha <mhafidza528@gmail.com>
2025-01-01 10:11:34 +0800

    Implement mock spy sleeper in test

--- Mocking/countdown_test.go
+++ Mocking/countdown_test.go
@@ -19,8 +19,9 @@ func (s *SpySleeper) Sleep() {
 
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -30,5 +31,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
 	}
 }
