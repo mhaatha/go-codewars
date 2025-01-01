ada98f11c9225b4ed7a6202512f4c5abdd1ec53b
mhaatha <mhafidza528@gmail.com>
2025-01-01 11:05:57 +0800

    Implements testing that can handle important behaviour of Countdown function

--- Mocking/sleeper.go
+++ Mocking/sleeper.go
@@ -19,3 +19,20 @@ type DefaultSleeper struct{}
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
+
+// Function to record all of the dependencies above into one list
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
