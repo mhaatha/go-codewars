73b670952bbb5f6b513715bb2ffa44573511eb38
mhaatha <mhafidza528@gmail.com>
2025-01-09 17:14:30 +0800

    Make the function test pass

--- Reflection/reflection_test.go
+++ Reflection/reflection_test.go
@@ -35,6 +35,11 @@ func walk(x interface{}, fn func(input string)) {
 				break
 			}
 		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
 	}
 }
 
