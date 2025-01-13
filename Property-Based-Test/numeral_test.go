83ae35ec304d5c500180e1052687a1ec99c9407f
mhaatha <mhafidza528@gmail.com>
2025-01-13 11:47:09 +0800

    Start with atomic test and make it pass

---
+++ Property-Based-Test/numeral_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestRomanNumeral(t *testing.T) {
+	got := ConvertToRoman(1)
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
