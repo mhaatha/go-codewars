adab79c7b55213ec43c9bb4c791ce93d091ca9e0
mhaatha <mhafidza528@gmail.com>
2025-01-13 13:22:34 +0800

    Refactor the ConvertToRoman function to not make it broke the builder

--- Property-Based-Test/numeral.go
+++ Property-Based-Test/numeral.go
@@ -3,13 +3,13 @@ package main
 import "strings"
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
 
 	for i := 0; i < arabic; i++ {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
