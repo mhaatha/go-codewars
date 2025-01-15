5871782b6668995fe563e18cfc1c720da07f90a7
mhaatha <mhafidza528@gmail.com>
2025-01-15 17:26:08 +0800

    Create comment on those functions

--- Maths/clockface.go
+++ Maths/clockface.go
@@ -17,8 +17,21 @@ type Point struct {
 	Y float64
 }
 
-// SecondHand is the unit vector of the second hand of an analogue clock at time `t`
-// represented as a Point.
+/*
+SecondHand is the unit vector of the second hand of an analogue clock at time `t`
+represented as a Point.
+
+scale -> Multiply the cos and sin from secondHandPoint with the length of second hands
+For example: p = (0.5 * 90, 0.866 * 90)
+p = Point{X: 45, Y: 77.94}
+
+flip -> Since the SVG system has an inverted Y axis (0 starts at the top), the Y coordinates are reversed.
+For example: p = Point{X: 45, Y: -77.94}
+
+translate -> Moves the needle position from the origin (0,0) to the hour center (150, 150)
+For example: p = (45 + 150, -77.94 + 150)
+p = Point{X: 195, Y: 72.06}
+*/
 func SecondHand(t time.Time) Point {
 	p := secondHandPoint(t)
 	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
@@ -27,6 +40,12 @@ func SecondHand(t time.Time) Point {
 	return p
 }
 
+/*
+secondHandPoint turns the radians into sin and cos
+
+x = sin(radians) -> Give the horizontal position of X axis
+y = cos(radians) -> Give the vertical position of Y axis
+*/
 func secondHandPoint(t time.Time) Point {
 	angle := secondsInRadians(t)
 	x := math.Sin(angle)
@@ -35,7 +54,15 @@ func secondHandPoint(t time.Time) Point {
 	return Point{x, y}
 }
 
-// Turns t into radiant, 2pi is same like 360 degree, math.Pi is same like 180 degree
+/*
+secondsInRadians turns t into radian
+
+1 second is 2π/60 radians.. cancel out the 2 and we get π/30 radians
+OLD FORMULA: radians = t.Second() * (pi / 30)
+
+but since float is horrible, we can write the formula as
+NEW FORMULA: radians = π / (30 / t.Second())
+*/
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
