570287fffc01c97f7b48a0d6ef2a6952a1963da8
mhaatha <mhafidza528@gmail.com>
2025-01-27 14:06:27 +0800

    Refactor Sum and SumAllTails to implement generics function Reduce

--- Array/sum.go
+++ Array/sum.go
@@ -1,11 +1,17 @@
 package array
 
-func Sum(numbers []int) int {
-	var sum int
-	for _, number := range numbers {
-		sum += number
+func Reduce[A any](collection []A, f func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = f(result, x)
 	}
-	return sum
+	return result
+}
+
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(numbers, add, 0)
 }
 
 func SumAll(numbersToSum ...[]int) []int {
@@ -17,16 +23,16 @@ func SumAll(numbersToSum ...[]int) []int {
 	return sums
 }
 
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
 func SumAllTails(numbersToSum ...[]int) []int {
-	var sums []int
-	for _, numbers := range numbersToSum {
-		if len(numbers) == 0 {
-			sums = append(sums, 0)
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tail := numbers[1:]
-			sums = append(sums, Sum(tail))
+			tail := x[1:]
+			return append(acc, Sum(tail))
 		}
 	}
 
-	return sums
+	return Reduce(numbersToSum, sumTail, []int{})
 }
